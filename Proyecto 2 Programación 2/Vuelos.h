9774a7953d45d120abae4a5052f25f6614f63297
Dixongn <108032347+Dixongn@users.noreply.github.com>
2022-06-29 22:20:45 -0600

    Update Vuelos.h

--- Proyecto 2 Programación 2/Vuelos.h
+++ Proyecto 2 Programación 2/Vuelos.h
@@ -14,7 +14,7 @@ private:
 
 public:
 
-	Vuelos() {
+	Vuelos() {  //Constructor
 		lugarOrigen = "";
 		lugarDestino = "";
 		tiempoTrayectoria = "";
@@ -23,7 +23,7 @@ public:
 
 	}
 
-	Vuelos(string nLugarOrigen, string nLugarDestino, string nTiempoTrayectoria, string nCostoVuelos, string nIdVuelo) {
+	Vuelos(string nLugarOrigen, string nLugarDestino, string nTiempoTrayectoria, string nCostoVuelos, string nIdVuelo) {   //contructor con parametros.
 		setLugarOrigen(nLugarOrigen);
 		setLugarDestino(nLugarDestino);
 		setTiempoTrayectoria(nTiempoTrayectoria);
@@ -36,57 +36,57 @@ public:
 
 
 
-	void setLugarOrigen(string nLugarOrigen) {
+	void setLugarOrigen(string nLugarOrigen) {   // Este Metodo se encarga de crea el set lugarOrigen.
 		this->lugarOrigen = nLugarOrigen;
 	}
 
-	string getLugarOrigen() {
+	string getLugarOrigen() {   // Este Metodo se encarga de crear el get de lugarOrigen.
 		return this->lugarOrigen;
 	}
 
 
-	void setLugarDestino(string nLugarDestino) {
+	void setLugarDestino(string nLugarDestino) {   // Este Metodo se encarga de crea el set lugarDestino
 		this->lugarDestino = nLugarDestino;
 	}
 
-	string getLugarDestino() {
+	string getLugarDestino() {   // Este Metodo se encarga de crear el get de lugarDestino
 		return this->lugarDestino;
 	}
 
 
-	void setTiempoTrayectoria(string nTiempoTrayectoria) {
+	void setTiempoTrayectoria(string nTiempoTrayectoria) {  // Este Metodo se encarga de crea el set de tiepotrayectoria
 		this->tiempoTrayectoria = nTiempoTrayectoria;
 	}
 
-	string getTiempoTrayectoria() {
+	string getTiempoTrayectoria() {   // Este Metodo se encarga de crear el get de tiempotrayectoria
 		return this->tiempoTrayectoria;
 	}
 
 
 
-	void setCostoVuelos(string nCostoVuelos) {
+	void setCostoVuelos(string nCostoVuelos) {   // Este Metodo se encarga de crea el set costoVuelos
 		this->costoVuelos = nCostoVuelos;
 	}
 
-	string getCostoVuelos() {
+	string getCostoVuelos() {   // Este Metodo se encarga de crear el get de vuelos
 		return this->costoVuelos;
 	}
 
 
-	void setIdVuelos(string nIdVuelos) {
+	void setIdVuelos(string nIdVuelos) {   // Este Metodo se encarga de crea el set de Idvuelos
 		this->idVuelo = nIdVuelos;
 	}
 
-	string getIdVuelos() {
+	string getIdVuelos() {   //Este Metodo se encarga de crear el get de idvuelos
 		return this->idVuelo;
 	}
 
 
-	void mostrarDatosVuelos() {
+	void mostrarDatosVuelos() {     // Metodo para mostrar la informacion.
 		cout << "Lugar de Origen: " << getLugarOrigen() << "	Lugar de Destino: " << getLugarDestino() << "	Tiempo de Trayectoria: " << getTiempoTrayectoria() << "	  Costo de Vuelo: " << getCostoVuelos() << "	Id de los vuelo: " << getIdVuelos() << endl << endl;
 	}
 
 
 
-	~Vuelos() {}
+	~Vuelos() {}   //destructor
 };
